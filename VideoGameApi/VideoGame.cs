e912cd5ce001c24a967bb4fa0dd5eb7e2b45c35f
andilebaselinedigital <andile@baselinedigital.co.za>
2025-07-09 12:45:59 +0200

    Add project files.

---
+++ VideoGameApi/VideoGame.cs
@@ -0,0 +1,13 @@
+﻿namespace VideoGameApi
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public int Plartform { get; set; }
+        public int Developer { get; set; }
+        public int Publisher { get; set; }
+
+
+    }
+}
